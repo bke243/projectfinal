e452a1589fbc9fa61d2436dc4a332bfa2d1230e2
peter <peterbadibake@gmail.com>
2019-09-17 12:00:23 +0200

    removing the last column time and see

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ def register():
             max_id = db.execute("SELECT MAX(id) FROM users")
             db_max_id = max_id[0].get("max")
             new_user_id = db_max_id + 1
-            db.execute("INSERT INTO users(id, username, email, hash, datetime) VALUES(:id, :username, :email, :hash_password)", id=new_user_id, username=username, email=usermail, hash_password=hash_password)
+            db.execute("INSERT INTO users(id, username, email, hash) VALUES(:id, :username, :email, :hash_password)", id=new_user_id, username=username, email=usermail, hash_password=hash_password)
             # store the user's id into session for a better user experience
             user = db.execute("SELECT id  FROM users WHERE username = :username", username=username)
             if not user:
